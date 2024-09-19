b9645330137079f082952792e23a26f3eed8d521
Bruno Sofiato <bruno.sofiato@matera.com>
2024-09-19 13:42:57 -0300

    [Cherry picked from v1.23.0 - Tentative] Included tag search capabilities (#32045) (#3)
    Resolves #31998
    
    The first screenshot shows the tag page without any filter being
    applied:
    
    ![image](https://github.com/user-attachments/assets/eac0e51c-9e48-42b2-bb1c-a25896ca40cb)
    
    The second one, shows the page when the given filter returns no tag:
    
    ![image](https://github.com/user-attachments/assets/98df191e-1a7b-4947-b0ef-4987a0293c3e)
    
    The last one shows a single tag being filtered:
    
    ![image](https://github.com/user-attachments/assets/79c7e05e-8c86-4f06-b17e-15818b7b9291)
    
    Signed-off-by: Bruno Sofiato <bruno.sofiato@gmail.com>
    Co-authored-by: Bruno Sofiato <bruno.sofiato@gmail.com>

--- models/repo/release.go
+++ models/repo/release.go
@@ -235,6 +235,7 @@ type FindReleasesOptions struct {
 	IsDraft       optional.Option[bool]
 	TagNames      []string
 	HasSha1       optional.Option[bool] // useful to find draft releases which are created with existing tags
+	NamePattern   optional.Option[string]
 }
 
 func (opts FindReleasesOptions) ToConds() builder.Cond {
@@ -262,6 +263,11 @@ func (opts FindReleasesOptions) ToConds() builder.Cond {
 			cond = cond.And(builder.Eq{"sha1": ""})
 		}
 	}
+
+	if opts.NamePattern.Has() && opts.NamePattern.Value() != "" {
+		cond = cond.And(builder.Like{"lower_tag_name", strings.ToLower(opts.NamePattern.Value())})
+	}
+
 	return cond
 }
 
