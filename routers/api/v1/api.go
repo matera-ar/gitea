24be2e007d10387e97febc5d0113044cd89a8542
Bruno Sofiato <bruno.sofiato@matera.com>
2024-12-18 17:17:37 -0300

    Inclusion of repo convert api

--- routers/api/v1/api.go
+++ routers/api/v1/api.go
@@ -1263,6 +1263,7 @@ func Routes() *web.Route {
 					})
 				}, reqRepoReader(unit.TypeReleases))
 				m.Post("/mirror-sync", reqToken(), reqRepoWriter(unit.TypeCode), mustNotBeArchived, repo.MirrorSync)
+				m.Post("/mirror-convert", reqToken(), reqRepoWriter(unit.TypeCode), mustNotBeArchived, repo.Convert)
 				m.Post("/push_mirrors-sync", reqAdmin(), reqToken(), mustNotBeArchived, repo.PushMirrorSync)
 				m.Group("/push_mirrors", func() {
 					m.Combo("").Get(repo.ListPushMirrors).
