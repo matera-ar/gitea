69000306fd0b14fd38d359a98899f37fff48f8f6
Bruno Sofiato <bruno.sofiato@matera.com>
2024-12-26 16:28:46 -0300

    Inclusion of rename organization api (to-be included on gitea)

--- routers/api/v1/api.go
+++ routers/api/v1/api.go
@@ -1513,6 +1513,7 @@ func Routes() *web.Route {
 			m.Combo("").Get(org.Get).
 				Patch(reqToken(), reqOrgOwnership(), bind(api.EditOrgOption{}), org.Edit).
 				Delete(reqToken(), reqOrgOwnership(), org.Delete)
+			m.Post("/rename", reqToken(), reqOrgOwnership(), bind(api.RenameOrgOption{}), org.Rename)
 			m.Combo("/repos").Get(user.ListOrgRepos).
 				Post(reqToken(), bind(api.CreateRepoOption{}), repo.CreateOrgRepo)
 			m.Group("/members", func() {
