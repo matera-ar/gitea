9607aa9ed31dbd01deb502df748bf8f48bfd6ccf
Bruno Sofiato <bruno.sofiato@matera.com>
2025-04-23 14:52:41 -0300

    Migrations adjustments

--- models/migrations/migrations.go
+++ models/migrations/migrations.go
@@ -356,19 +356,19 @@ func prepareMigrationTasks() []*migration {
 
 		// Gitea 1.22.0-rc1 ends at migration ID number 298 (database version 299)
 
-		newMigration(299, "Add content version to issue and comment table", v1_23.AddContentVersionToIssueAndComment),
-		newMigration(300, "Add force-push branch protection support", v1_23.AddForcePushBranchProtection),
-		newMigration(301, "Add skip_secondary_authorization option to oauth2 application table", v1_23.AddSkipSecondaryAuthColumnToOAuth2ApplicationTable),
-		newMigration(302, "Add index to action_task stopped log_expired", v1_23.AddIndexToActionTaskStoppedLogExpired),
-		newMigration(303, "Add metadata column for comment table", v1_23.AddCommentMetaDataColumn),
-		newMigration(304, "Add index for release sha1", v1_23.AddIndexForReleaseSha1),
-		newMigration(305, "Add Repository Licenses", v1_23.AddRepositoryLicenses),
-		newMigration(306, "Add BlockAdminMergeOverride to ProtectedBranch", v1_23.AddBlockAdminMergeOverrideBranchProtection),
-		newMigration(307, "Fix milestone deadline_unix when there is no due date", v1_23.FixMilestoneNoDueDate),
-		newMigration(308, "Add index(user_id, is_deleted) for action table", v1_23.AddNewIndexForUserDashboard),
-		newMigration(309, "Improve Notification table indices", v1_23.ImproveNotificationTableIndices),
-		newMigration(310, "Add Priority to ProtectedBranch", v1_23.AddPriorityToProtectedBranch),
-		newMigration(311, "Add TimeEstimate to Issue table", v1_23.AddTimeEstimateColumnToIssueTable),
+		newMigration(300, "Add content version to issue and comment table", v1_23.AddContentVersionToIssueAndComment),
+		newMigration(301, "Add force-push branch protection support", v1_23.AddForcePushBranchProtection),
+		newMigration(302, "Add skip_secondary_authorization option to oauth2 application table", v1_23.AddSkipSecondaryAuthColumnToOAuth2ApplicationTable),
+		newMigration(303, "Add index to action_task stopped log_expired", v1_23.AddIndexToActionTaskStoppedLogExpired),
+		newMigration(304, "Add metadata column for comment table", v1_23.AddCommentMetaDataColumn),
+		newMigration(305, "Add index for release sha1", v1_23.AddIndexForReleaseSha1),
+		newMigration(306, "Add Repository Licenses", v1_23.AddRepositoryLicenses),
+		newMigration(307, "Add BlockAdminMergeOverride to ProtectedBranch", v1_23.AddBlockAdminMergeOverrideBranchProtection),
+		newMigration(308, "Fix milestone deadline_unix when there is no due date", v1_23.FixMilestoneNoDueDate),
+		newMigration(309, "Add index(user_id, is_deleted) for action table", v1_23.AddNewIndexForUserDashboard),
+		newMigration(310, "Improve Notification table indices", v1_23.ImproveNotificationTableIndices),
+		newMigration(311, "Add Priority to ProtectedBranch", v1_23.AddPriorityToProtectedBranch),
+		newMigration(312, "Add TimeEstimate to Issue table", v1_23.AddTimeEstimateColumnToIssueTable),
 	}
 	return preparedMigrations
 }
